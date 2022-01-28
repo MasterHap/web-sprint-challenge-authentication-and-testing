bc92748553903b770d519d83ff6e94290126e53d
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-01-28 17:04:42 -0500

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -54,7 +54,7 @@ router.post('/login', (req, res, next) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = makeToken(user)
         res.status(200).json({
-          message: `Welcome ${user.username}...`,
+          message: `welcome ${user.username}...`,
           token
         })
       } else {
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,21 @@
 // Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+const bcrypt = require('bcrypt')
+// const jwtDecode = require('jwt-decode')
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true)
+})
+describe('[POST] /auth/register', () => {
+  test('[1] saves the user with a bcrypted password instead of plain text', async () => {
+    await request(server).post('/auth/register').send({ username: 'jake', password: '1234' })
+    const jake = await db('users').where('username', 'jake').first()
+    expect(bcrypt.compareSync('1234', jake.password)).toBeTruthy()
+  }, 750)
+  test('[2] responds with proper status on success', async () => {
+    const res = await request(server).post('/auth/register').send({ username: 'jake', password: '1234' })
+    expect(res.status).toBe(201)
+  }, 750)
 })
